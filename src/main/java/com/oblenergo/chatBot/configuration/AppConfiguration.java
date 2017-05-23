86c55fce2c619be2055a6ab3a192710b53289b6b
yuralubinec <yuralubinec@gmail.com>
2017-05-23 08:57:14 +0300

    added cors configuration (for developing only)

---
+++ src/main/java/com/oblenergo/chatBot/configuration/AppConfiguration.java
@@ -0,0 +1,22 @@
+package com.oblenergo.chatBot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class AppConfiguration {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/customer/**").allowedMethods("GET", "POST", "OPTION").allowedOrigins("*");
+      }
+    };
+  }
+
+}
