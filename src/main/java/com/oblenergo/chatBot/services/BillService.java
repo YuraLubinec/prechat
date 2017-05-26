99ac322fc475a658dc8741c0ddbd79a16d7739ba
yuralubinec <yuralubinec@gmail.com>
2017-05-26 12:34:42 +0300

    added checking day of month when saving indicator

--- src/main/java/com/oblenergo/chatBot/services/BillService.java
+++ src/main/java/com/oblenergo/chatBot/services/BillService.java
@@ -1,5 +1,6 @@
 package com.oblenergo.chatBot.services;
 
+import java.time.LocalDate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,11 +13,20 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class BillService {
-  
+
   private static final String REGEXPATTERN = "amountToPay=\"[+-]?(\\d+)([\\.]?)(\\d*)";
+  private static final int LAST_MONTH_OF_THE_YEAR = 12;
+  private static final int LENTH_TO_TRIM = 13;
+  private static final int FIRST_MONTH_OF_THE_YEAR = 1;
 
   @Value("${bill.service.url}")
   private String url;
+
+  @Value("${begin.period.date}")
+  private int beginPeriodDate;
+
+  @Value("${end.period.date}")
+  private int endPeriodDate;
 
   @Value("${xmlToSendFirstPart}")
   private String xmlToSendFirstPart;
@@ -25,19 +35,40 @@ public class BillService {
   private String xmlToSendSecondPart;
 
   public ResponseEntity<String> getBill(String accountNumber) {
-    RestTemplate template = new RestTemplate();
-    HttpEntity<String> request = new HttpEntity<String>(xmlToSendFirstPart + accountNumber + xmlToSendSecondPart);
-    return new ResponseEntity<String>(parseResponse(template.postForObject(url, request, String.class)), HttpStatus.OK);
+
+    if (checkPeriod()) {
+      RestTemplate template = new RestTemplate();
+      HttpEntity<String> request = new HttpEntity<String>(xmlToSendFirstPart + accountNumber + xmlToSendSecondPart);
+      return new ResponseEntity<String>(parseResponse(template.postForObject(url, request, String.class)),
+          HttpStatus.OK);
+    }
+    return new ResponseEntity<String>("Avaialble only from 20 to 3 of the month", HttpStatus.OK);
   }
-  
-  private String parseResponse(String response){
+
+  private String parseResponse(String response) {
+
     Pattern p = Pattern.compile(REGEXPATTERN);
     Matcher m = p.matcher(response);
     String token = null;
     while (m.find()) {
-      token=m.group().substring(13);
+      token = m.group().substring(LENTH_TO_TRIM);
     }
     return token;
+  }
+
+  private boolean checkPeriod() {
+
+    LocalDate localDateNow = LocalDate.now();
+    int month = localDateNow.getMonthValue();
+    if (localDateNow.getMonthValue() == LAST_MONTH_OF_THE_YEAR) {
+      LocalDate endDateExclusive = LocalDate.of(localDateNow.getYear(), FIRST_MONTH_OF_THE_YEAR, endPeriodDate);
+      LocalDate startDateInclusive = LocalDate.of(localDateNow.getYear(), month, beginPeriodDate);
+      return localDateNow.isBefore(endDateExclusive) && localDateNow.isAfter(startDateInclusive);
+    } else {
+      LocalDate endDateExclusive = LocalDate.of(localDateNow.getYear(), month + 1, endPeriodDate);
+      LocalDate startDateInclusive = LocalDate.of(localDateNow.getYear(), month, beginPeriodDate);
+      return localDateNow.isBefore(endDateExclusive) && localDateNow.isAfter(startDateInclusive);
+    }
   }
 
 }
